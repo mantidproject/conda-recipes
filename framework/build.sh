b444f3f49c4bb615c9e53d895f5c3418d1ae7252
Jiao Lin <linjiao@ornl.gov>
2016-07-27 14:59:12 -0400

    Refs #6. first attempt.

--- framework/build.sh
+++ framework/build.sh
@@ -7,42 +7,13 @@ if ((CORES < 1)); then
 fi
 
 # ----------------------------------------------------------------------
-# deps
-mkdir -p deps
-cd deps
-
-# poco 1.7.3
-wget http://pocoproject.org/releases/poco-1.7.3/poco-1.7.3-all.tar.gz
-tar xvfz poco-1.7.3-all.tar.gz
-cd poco-1.7.3-all
-./configure --prefix=$PREFIX && make -j $CORES && make install 
-cd ..
-
-# neuxs 4.3.1
-wget https://github.com/nexusformat/code/releases/download/4.3.1/nexus-4.3.1.tar.gz
-tar xvfz nexus-4.3.1.tar.gz
-cd nexus-4.3.1
-CFLAGS="-I $PREFIX/include" ./configure --prefix=$PREFIX --with-hdf5=$PREFIX; make -j $CORES; make install
-cd ..
-
-# jsoncpp 0.10.6
-wget https://github.com/open-source-parsers/jsoncpp/archive/0.10.6.tar.gz -O jsoncpp-0.10.6.tar.gz
-tar xvfz jsoncpp-0.10.6.tar.gz
-cd jsoncpp-0.10.6
-mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS=ON .. &&  make -j $CORES && make install
-cd .. # back out from build
-cd .. # back to deps
-cd .. # back
-
-# ----------------------------------------------------------------------
 # update cmake files
 git clone https://github.com/mantidproject/conda-recipes mantid-conda-recipes
 rsync -av ./mantid-conda-recipes/framework/revised/ ./
 
 # ----------------------------------------------------------------------
 mkdir build; cd build
-cmake -DBOOST_ROOT=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib -DNEXUS_LIBRARIES=$PREFIX/lib -DNEXUS_INCLUDE_DIR=$PREFIX/include -DHDF5_DIR=$PREFIX -DMUPARSER_INCLUDE_DIR=$PREFIX/include  -DENABLE_OPENCASCADE= -DVERSION_PATCH=100  ../Framework/
+cmake -DBOOST_ROOT=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib -DNEXUS_LIBRARIES=$PREFIX/lib -DNEXUS_INCLUDE_DIR=$PREFIX/include -DHDF5_DIR=$PREFIX -DMUPARSER_INCLUDE_DIR=$PREFIX/include  -DENABLE_OPENCASCADE= -DVERSION_PATCH=0  ../Framework/
 make -j $CORES
 make install
 
