fd2fe483e7ab6e8fd15df447f1cd365e976e2bfe
Pete Peterson <petersonpf@ornl.gov>
2016-08-04 16:52:17 -0400

    Various cmake fixes

--- framework/build.sh
+++ framework/build.sh
@@ -6,6 +6,22 @@ if ((CORES < 1)); then
     CORES = 1;
 fi
 
+if [ $(command -v cmake3) ]; then
+    CMAKE=$(command -v cmake3)
+else
+    CMAKE=$(command -v cmake)
+fi
+
+if [ $(command -v ninja) ]; then
+  CMAKE_GENERATOR="-G Ninja"
+elif [ $(command -v ninja-build) ]; then
+  CMAKE_GENERATOR="-G Ninja"
+fi
+if [ -e CMakeCache.txt ]; then
+  CMAKE_GENERATOR=""
+fi
+
+
 # ----------------------------------------------------------------------
 # update cmake files
 git clone https://github.com/mantidproject/conda-recipes mantid-conda-recipes
@@ -13,9 +29,13 @@ rsync -av ./mantid-conda-recipes/framework/revised/ ./
 
 # ----------------------------------------------------------------------
 mkdir build; cd build
-cmake -DBOOST_ROOT=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib -DNEXUS_LIBRARIES=$PREFIX/lib -DNEXUS_INCLUDE_DIR=$PREFIX/include -DHDF5_DIR=$PREFIX -DMUPARSER_INCLUDE_DIR=$PREFIX/include  -DENABLE_OPENCASCADE= -DVERSION_PATCH=0  ../Framework/
-make -j $CORES
-make install
+${CMAKE} ${CMAKE_GENERATOR} -DBOOST_ROOT=$PREFIX -DENABLE_MANTIDPLOT=FALSE -DCMAKE_INSTALL_PREFIX=$PREFIX \
+         -DCMAKE_SYSTEM_LIBRARY_PATH=$PREFIX/lib -DNEXUS_LIBRARIES=$PREFIX/lib -DNEXUS_INCLUDE_DIR=$PREFIX/include \
+         -DHDF5_DIR=$PREFIX -DMUPARSER_INCLUDE_DIR=$PREFIX/include  -DENABLE_OPENCASCADE= \
+         -DOPENSSL_INCLUDE_DIR=$PREFIX/include -DOPENSSL_CRYPTO_LIBRARY=$PREFIX/lib/libcrypto.so \
+         -DOPENSSL_SSL_LIBRARY=$PREFIX/lib/libssl.so ../
+${CMAKE} --build . -- -j $CORES
+${CMAKE} --build . --target install
 
 # move mantid
 python_site_pkg_path=`python -c "import h5py, os; opd=os.path.dirname; print opd(opd(h5py.__file__))"`
