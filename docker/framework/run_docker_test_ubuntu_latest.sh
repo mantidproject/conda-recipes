a89455826c9cfef9f390cf51bb5957137a9ea2c0
Jiao Lin <jiao.lin@gmail.com>
2017-04-24 14:17:45 -0400

    Refs #16. made ubuntu docker test more consistent with centos6 docker test

--- docker/framework/run_docker_test_ubuntu_latest.sh
+++ docker/framework/run_docker_test_ubuntu_latest.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
+WORK=$(cd "$(dirname "$0")"; pwd;)
+
+# In order for the conda-build process in the container to write to the mounted 
+# volumes, we need to run with the same id as the host machine, which is        
+# normally the owner of the mounted volumes, or at least has write permission  
+HOST_USER_ID=$(id -u)
+# Check if docker-machine is being used (normally on OSX) and get the uid from
+# the VM
+if hash docker-machine 2> /dev/null && docker-machine active > /dev/null; then
+    HOST_USER_ID=$(docker-machine ssh $(docker-machine active) id -u)
+fi
+
+
 cat << EOF | docker run \
-  --net=host -i\
-  -a stdin -a stdout -a stderr \
-  -e HOST_USER_ID=lj7 \
-  ubuntu \
-  bash || exit $?
+                    --net=host -i\
+                    -v ${WORK}:/work \
+                    -a stdin -a stdout -a stderr \
+                    -e HOST_USER_ID=${HOST_USER_ID} \
+                    ubuntu \
+                    bash -ex || exit $?
 
 apt-get update --fix-missing && apt-get install -y wget bzip2 ca-certificates \
     libglib2.0-0 libxext6 libsm6 libxrender1 \
@@ -23,9 +37,11 @@ conda config --add channels conda-forge
 conda config --add channels mantid
 conda config --set always_yes true
 
+apt-get install -y freeglut3-dev
+
 conda install --quiet --yes -c mantid/label/candidate mantid-framework
 
-apt-get install -y freeglut3-dev
+python -c "import mantid" >/work/log.test-ubuntu-latest-first-import 2>&1 || echo "first import errored"
 
 python -c "import mantid; print(mantid.__version__)"
 
