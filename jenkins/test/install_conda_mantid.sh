971f8fdb4a4ee90fbf29da23f4af68b58513e0ff
Jiao Lin <linjiao@ornl.gov>
2017-05-10 14:25:04 -0400

    Refs #19. installation step now involves uploading tar ball to anaconda, and then install

--- jenkins/test/install_conda_mantid.sh
+++ jenkins/test/install_conda_mantid.sh
@@ -5,10 +5,17 @@ bash miniconda.sh -b -p $HOME/opt/miniconda2
 export PATH=$HOME/opt/miniconda2/bin:$PATH
 conda config --add channels conda-forge
 conda config --add channels mantid
+
+## anaconda client
+conda install -n root --yes --quiet anaconda-client
+anaconda login --username mantid --password $ANACONDA_PASSWORD
+
+## upload conda pkg to mantid jenkins channel
+anaconda upload -l jenkins --force $1
+
 ## Establish mantid environment
-conda create --yes --name mantid python=2
+conda create --yes --quiet --name mantid python=2
+## install
 source activate mantid
 conda install --yes --quiet numpy
-conda install --yes --quiet mantid-framework
-conda remove --yes --quiet mantid-framework
-conda install --yes --quiet $1
+conda install --yes --quiet -c mantid/label/jenkins mantid-framework
