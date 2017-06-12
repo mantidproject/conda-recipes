48e873ea0fcdd597c86ec4852f0485acd66dc476
Jiao Lin <linjiao@ornl.gov>
2017-06-12 11:42:17 -0400

    Refs #16, #19. jenkins/upload.sh: fixing a bug

--- jenkins/upload.sh
+++ jenkins/upload.sh
@@ -3,7 +3,7 @@
 # Usage: ./upload.sh  -l nightly --force /path/to/pkg.bz2
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/setup_mc2.sh
+. $DIR/setup_mc2.sh
 source activate build
 
 # anaconda settings
