2c0978826e471563bb2b48932ea9248ecfcd331a
Jiao Lin <jiao.lin@gmail.com>
2017-10-27 14:39:04 -0400

    Refs mantidproject/mantid#21068. update nexus recipe

--- nexus/build.sh
+++ nexus/build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-let CORES=`grep -c ^processor /proc/cpuinfo`
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)
+	let CORES=`grep -c ^processor /proc/cpuinfo`
+	;;
+    Darwin*)
+	let CORES=`sysctl -n hw.ncpu`
+	export CXXFLAGS="-stdlib=libc++ -std=c++11 -Wno-narrowing"
+	;;
+    *)  echo "${unameOut} unsupported"; exit 1
+esac
 let CORES-=1
 if ((CORES < 1)); then
     CORES = 1;
