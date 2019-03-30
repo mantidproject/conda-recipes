eb2b9eca1412390c26e64f80d84732b697dacb39
Jiao Lin <linjiao@ornl.gov>
2019-03-30 12:20:40 -0400

    added docker/framework/run_docker_jenkinssystemtests_continuumio-mc2.sh

---
+++ docker/framework/run_docker_jenkinssystemtests_continuumio-mc2.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# Script to run jenkins systemtests for mantid-framework conda package using a docker container
+# assume
+# 1) mantid src tree exists
+# 2) mantidexternaldata directory exists
+# 3) a mantid-framework conda package
+#
+# Example command line
+#
+#   $ BUILD_THREADS=20 ./run_docker_jenkinssystemtests_continuumio-mc2.sh build_artefacts2/linux-64/mantid-framework-3.13.20190225.1515-py27h5d0adaf_0.tar.bz2  /home/lj7/dv/mantid/mantid ~/MantidExternalData &>log.test &
+# 
+# see results in $MANTID_SRCROOT/build/Testing/SystemTests/scripts/TEST-*.xml
+
+MANTID_CONDA_TARBALL=$1
+MANTID_SRCROOT=$2
+MANTID_EXTDATADIR=$3
+
+mkdir -p $MANTID_SRCROOT/build/
+cp $MANTID_CONDA_TARBALL $MANTID_SRCROOT/build/
+
+IMAGE_NAME="continuumio/miniconda2"
+owner=$(stat -c '%u:%g' ${MANTID_SRCROOT})
+
+cat << EOF | docker run -i \
+		    -v ${MANTID_SRCROOT}:/mantidsrc \
+		    -v ${MANTID_EXTDATADIR}:/mantidextdata \
+                    $IMAGE_NAME \
+                    bash -ex || exit $?
+
+set -e
+
+# Install OpenGL
+apt-get install -y freeglut3-dev make
+
+# Setup conda for build
+echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
+. ~/.bashrc
+conda config --set always_yes yes
+conda config --add channels conda-forge
+conda config --add channels mantid
+
+# Setup the conda environment
+ENV="mantid-systemtests"
+conda create -n \${ENV} -q python=2
+conda activate \${ENV}
+conda install cmake gxx_linux-64
+
+# external data
+ln -s /mantidextdata ~/MantidExternalData
+export MANTID_FRAMEWORK_CONDA_SYSTEMTEST=1
+export WORKSPACE=/mantidsrc
+ls /mantidsrc
+cd /mantidsrc
+export BUILD_THREADS=${BUILD_THREADS}
+./buildconfig/Jenkins/systemtests
+
+conda deactivate
+chown -R ${owner} /mantidsrc
+
+EOF
