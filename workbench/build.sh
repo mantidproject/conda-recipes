0230be823a9d8b8dedfef272670d70540980c44b
Jiao Lin <linjiao@ornl.gov>
2019-04-30 20:21:48 -0400

    trying to fix workbench build

--- workbench/build.sh
+++ workbench/build.sh
@@ -56,7 +56,7 @@ ${CMAKE} --build . --target install
 python_site_pkg_path=`python -c "from __future__ import print_function; import h5py, os; opd=os.path.dirname; print(opd(opd(h5py.__file__)))"`
 echo $python_site_pkg_path
 mv $PREFIX/bin/mantid $python_site_pkg_path/
-mkdir $PREFIX/lib/mantid
+mkdir -p $PREFIX/lib/mantid
 ln -s $PREFIX/plugins $PREFIX/lib/mantid/plugins
 
 # move other workbench related libraries
