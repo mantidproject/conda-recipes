5775dfdc48a0df353e456518f77e7d01e62d236d
Pete Peterson <petersonpf@ornl.gov>
2020-04-30 16:44:46 -0400

    Remove unused framework build scripts

--- docker/README.md
+++
@@ -1,4 +0,0 @@
-# Docker utils for building mantid packages
-
-Eventually this should be migrated to conda-forge.
-
--- docker/framework/explore/start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --net=host -it -v $PWD/../../..:/work -e HOST_USER_ID=lj7 condaforge/linux-anvil
