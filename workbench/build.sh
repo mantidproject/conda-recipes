d661d8b5a2df7746f0237803ad6d6d1f01cd9003
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-05-07 23:13:43 -0400

    Initial refactor of workbench recipe

--- workbench/build.sh
+++ workbench/build.sh
@@ -5,6 +5,17 @@ set -ex
 mkdir build
 cd build
 
+# Mesa libGL paths for the Conda CDT (Core Dependency Tree) libraries
+# See http://conda-forge.org/docs/maintainer/knowledge_base.html?highlight=libgl#libgl
+#
+# NOTE: These are variables for Legacy OpenGL (see OpenGL_GL_PREFERENCE below)
+# See https://cmake.org/cmake/help/v3.15/module/FindOpenGL.html
+#
+export CDT_MESA_LIBGL_DIR="${CONDA_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr"
+export OPENGL_gl_LIBRARY="${CDT_MESA_LIBGL_DIR}/libGL.so.1"
+export OPENGL_INCLUDE_DIR="${CDT_MESA_LIBGL_DIR}/include/GL"
+export OPENGL_glu_LIBRARY="${CONDA_PREFIX}/lib/libGLU.so.1"
+
 cmake \
    -G Ninja \
   -DUSE_SYSTEM_EIGEN=ON \
@@ -17,6 +28,7 @@ cmake \
   -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
   -DCMAKE_SKIP_INSTALL_RPATH=OFF \
   -DBOOST_INCLUDEDIR="$CONDA_PREFIX/include" \
+  -DOpenGL_GL_PREFERENCE="LEGACY" \
   ../
 
 cmake --build .
