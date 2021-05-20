0ff548be0e6e1758ed6a21f9333a808e02a0c6cd
Pete Peterson <petersonpf@ornl.gov>
2021-05-20 10:28:21 -0400

    Improve error reporting

--- jenkins/upload.sh
+++ jenkins/upload.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-set -x
 
 # Usage: ./upload.sh  -l nightly --force /path/to/pkg.bz2
+# anaconda arguments are https://docs.anaconda.com/anacondaorg/commandreference/
 
-# conda
-# MC_DIR=$HOME/miniconda2  
+# verify that the upload token is supplied
+if [ -z "$ANACONDA_ACCESS_KEY" ]; then
+  echo "ANACONDA_ACCESS_KEY is not set"
+  exit 255
+fi
+
+# add location of anaconda to the path
+echo "Adding $MC_DIR/bin to the PATH"  
 export PATH=$MC_DIR/bin:$PATH
 
+if [ ! $(command -v anaconda) ]; then
+  echo "Failed to find anaconda command"
+  exit 255
+fi
+
 # upload
-# the tool arguments are https://docs.anaconda.com/anacondaorg/commandreference/
 anaconda upload -t $ANACONDA_ACCESS_KEY $@
