00fd5f2708718a2199f3da3be6e2cfaea437685e
Pete Peterson <petersonpf@ornl.gov>
2021-05-27 10:31:16 -0400

    Activate conda rather than calling executables direcly

--- jenkins/upload.sh
+++ jenkins/upload.sh
@@ -10,18 +10,31 @@ if [ -z "$ANACONDA_ACCESS_KEY" ]; then
   exit 255
 fi
 
+# activate anaconda
+if [ -f "$MC_DIR/bin/activate" ]; then
+  echo "Activate anacconda"
+  source "$MC_DIR/bin/activate"
+else
+  echo "Failed to find anaconda install"
+  exit 255
+fi
+
 # add location of anaconda to the path
-echo "Adding $MC_DIR/bin to the PATH"  
-export PATH=$MC_DIR/bin:$PATH
+#echo "Adding $MC_DIR/bin to the PATH"  
+#export PATH=$MC_DIR/bin:$PATH
 
 if [ ! $(command -v anaconda) ]; then
   echo "Failed to find anaconda command"
   exit 255
 fi
 
+# tool was last updated using "conda install python=2.7.16 anaconda-client" in the "base" environment
 # show tool version
 anaconda --version
 
 # upload
 anaconda -v upload -t $ANACONDA_ACCESS_KEY $@ 2>&1
 echo "anaconda returned $?"
+
+# cleanup
+conda deactivate
